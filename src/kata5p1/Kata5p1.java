bc9a5d0e63ac814307d05ed4fe117d85102f4f54
usuario <usuario@10.22.148.65>
2017-11-17 14:02:10 +0000

    Segunda Versión

--- src/kata5p1/Kata5p1.java
+++ src/kata5p1/Kata5p1.java
@@ -4,11 +4,15 @@
  */
 package kata5p1;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.sql.Statement;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.io.*;
+import java.util.*;
 
 /**
  *
@@ -33,5 +37,31 @@ public class Kata5p1 {
             System.out.println(rs.getInt(1));
             System.out.println(rs.getString(2));
         }
+        
+        query = "CREATE TABLE IF NOT EXISTS MAIL ('Id' INTEGER " +
+            "PRIMARY KEY AUTOINCREMENT ,'Mail'	TEXT NOT NULL);"; 
+        st.execute(query);
+        FileReader fl = null;
+    BufferedReader bf = null;
+    String line = "C:\\Users\\usuario\\Desktop\\Kata4.txt";
+    
+    try{
+    fl = new FileReader(line);
+    bf = new BufferedReader (fl);
+    String lineaux;
+    while((lineaux = bf.readLine()) != null){
+        if(!lineaux.contains("@")){
+            continue;
+        }
+        query =	"INSERT	INTO	MAIL	(Mail)	VALUES	('"+lineaux+"');”";	
+        st.execute(query);
     }
+    }catch(IOException e){
+        e.printStackTrace();
+    }
+    query = "Select count (*) from MAIL";
+    rs = st.executeQuery(query);
+    System.out.println("Número de registros de la tabla MAIL: " + rs.getInt(1));
+  }
+    
 }
